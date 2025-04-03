eff5a7c6f3ce23d3d772ec8a06a79bba3ee2857a
PabloTec <36818514+dainnin@users.noreply.github.com>
2025-04-03 12:01:26 -0300

    Add files via upload

---
+++ req/asd.js
@@ -0,0 +1,20 @@
+import { $, proxyFlex, parseHTML, voidThis, fetchResReq } from '/modulos/funciones/utilidades.js';
+
+ const abc = new fetchResReq({ setGlobal: true });
+abc.setStatic(["https://dainnin.alwaysdata.net/api/checkToken", {
+  method: 'GET',
+  credentials: 'include', // Asegura que las cookies se envíen
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  "mode": "cors"
+}])
+
+
+
+ const abcPorxy = proxyFlex(abc,'fetchR');
+
+ export const ab = abcPorxy.proxy
+ export const setStateCheck=abcPorxy.suscribir
+
+ export const setStateArr = abcPorxy.suscriptores
