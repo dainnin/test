e001e3a0df67ed8ff64d24aee6b753d9d862307f
PabloTec <36818514+dainnin@users.noreply.github.com>
2025-04-05 11:23:19 -0300

    Update login.js

--- main/login.js
+++ main/login.js
@@ -2,39 +2,39 @@
 import {  HTMLatObj } from "/test/funciones/creacion.js"
 import { $ } from '/test/funciones/utilidades.js';
 import {ab} from '/test/req/asd.js'
-async function login (event) {
+async function login(event) {
     event.preventDefault()
-    const form=event.target.form
-    
-    const data= new FormData(form)
-    const body={}
- for(let [k,v] of data.entries()){
-    body[k]=v;
- }
+    const form = event.target.form
 
- if(event.target.tagName==="BUTTON" && Object.values(body).indexOf("")===-1){
-    const req = new $.test({});
+    const data = new FormData(form)
+    const body = {}
+    for (let [k, v] of data.entries()) {
+        body[k] = v;
+    }
 
-    await req.fetchE(["https://dainnin.alwaysdata.net/api/login", {
-    "credentials": "include",
-    "headers": {
+    if (event.target.tagName === "BUTTON" && Object.values(body).indexOf("") === -1) {
+        const req = new $.test({});
+
+         req.fetchE(["https://dainnin.alwaysdata.net/api/login", {
+            "credentials": "include",
+            "headers": {
+
+                "Accept": "application/json, text/plain, */*",
+                "Content-Type": "application/json",
+
+            },
+            "mode": "cors",
+            "body": JSON.stringify(body),
+            "method": "POST",
+
+        }]).then(()=>ab.fetchR)
+
         
-        "Accept": "application/json, text/plain, */*",
-        "Content-Type": "application/json",
-     
-    },
-    "mode": "cors",
-    "body": JSON.stringify(body),
-    "method": "POST",
-    
-    }])
-    
-    ab.fetchR
-   
-    
-}
 
-   
+
+    }
+
+
 }
 
 $.referencias(login)
