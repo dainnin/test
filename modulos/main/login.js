d5e795b4e7b2ca056608822892ad7cfbbfbec96a
PabloTec <36818514+dainnin@users.noreply.github.com>
2025-04-05 11:41:18 -0300

    Add files via upload

---
+++ modulos/main/login.js
@@ -0,0 +1,53 @@
+
+import { HTMLatObj } from "/modulos/funciones/creacion.js"
+import { $ } from '/modulos/funciones/utilidades.js';
+import { ab } from '/modulos/req/asd.js'
+async function login(event) {
+    event.preventDefault()
+    const form = event.target.form
+
+    const data = new FormData(form)
+    const body = {}
+    for (let [k, v] of data.entries()) {
+        body[k] = v;
+    }
+
+    if (event.target.tagName === "BUTTON" && Object.values(body).indexOf("") === -1) {
+        const req = new $.test({});
+
+         req.fetchE(["http://localhost:3069/api/login", {
+            "credentials": "include",
+            "headers": {
+
+                "Accept": "application/json, text/plain, */*",
+                "Content-Type": "application/json",
+
+            },
+            "mode": "cors",
+            "body": JSON.stringify(body),
+            "method": "POST",
+
+        }]).then(()=>ab.fetchR)
+
+        
+
+
+    }
+
+
+}
+
+$.referencias(login)
+export const LoginForm = () => HTMLatObj(`
+    <form onclick="login||F">
+            <label>
+                Usuario:
+                <input type="text" name="user"  required ></input>
+            </label>
+            <label>
+                Password:
+                <input type="password" name="password"  required></input>
+            </label>
+            <button >Iniciar sesión</button>
+    </form>`
+)
