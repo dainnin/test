0d6e9b9632fe26b3aa02704f35f8d2276fd1829e
PabloTec <36818514+dainnin@users.noreply.github.com>
2025-04-07 09:01:15 -0300

    Update creacion.js

--- modulos/funciones/creacion.js
+++ modulos/funciones/creacion.js
@@ -55,7 +55,7 @@ export const createUpdate = async (e, b) => {
         const eTag = Target.tagName
         const eHref = Target.href
         
-        if (eTag === 'A' || eTag === 'IMG' & Father.tagName === 'A') {
+        if (eTag === 'A' || eTag === 'IMG' && Father.tagName === 'A') {
             alert(Father.href.replace(location.origin,''))
             event.preventDefault()
             if (eTag === 'IMG' & Father.tagName === 'A' && eHref !== location.href && eHref !== $.QPPath(location, true).url) {
