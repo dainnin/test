cf2fdcc453d6fb24e507c61ae08f29dafad0df46
PabloTec <36818514+dainnin@users.noreply.github.com>
2025-04-08 07:42:11 -0300

    Update env.js

--- modulos/env.js
+++ modulos/env.js
@@ -1,10 +1,10 @@
 export const urls = {
-    online: {
+    local: {
         api: 'https://dainnin.alwaysdata.net/api/',
         app: 'https://dainnin.github.io/test/',
         
     },
-    local: {
+    online: {
         api: 'http://localhost:3069/api/',
         app: 'http://dainnin.github.io/test/',
       
